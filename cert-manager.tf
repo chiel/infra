08718250226c24ba4d605b1f4148bb1a39edb45f
Chiel Kunkels <chiel@kunkels.me>
2021-10-21 00:47:20 +0200

    add cert-manager helm chart

---
+++ cert-manager.tf
@@ -0,0 +1,23 @@
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+
+    labels = {
+      "app.kubernetes.io/managed-by" : "terraform"
+      "app.kubernetes.io/name" : "cert-manager"
+    }
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.5.4"
+  namespace  = kubernetes_namespace.cert_manager.metadata[0].name
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
