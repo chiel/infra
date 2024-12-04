5449eb8ad9edda05e112ecf1003d75ccc88b64a8
Chiel Kunkels <chiel@kunkels.me>
2024-12-04 23:56:56 +0100

    feat: create kubernetes cluster

---
+++ kubernetes.tf
@@ -0,0 +1,13 @@
+resource "digitalocean_kubernetes_cluster" "default" {
+  name    = "default"
+  region  = "ams3"
+  version = "1.31.1-do.4"
+
+  node_pool {
+    name       = "default-nodes"
+    size       = "s-1vcpu-2gb"
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 3
+  }
+}
