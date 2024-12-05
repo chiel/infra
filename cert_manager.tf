228ec9806d66d9b695ff456d477f0f0d40e44189
Chiel Kunkels <chiel@kunkels.me>
2024-12-05 01:14:28 +0100

    feat(k8s): install cert manager

---
+++ cert_manager.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+
+    labels = {
+      "app.kubernetes.io/name" : "cert-manager"
+    }
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.16.2"
+  namespace  = kubernetes_namespace.cert_manager.metadata[0].name
+
+  set {
+    name  = "crds.enabled"
+    value = "true"
+  }
+}
